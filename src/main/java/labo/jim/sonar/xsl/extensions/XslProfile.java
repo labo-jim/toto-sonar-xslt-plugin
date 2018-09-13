c55e613170bcd30758d36e7a04df338fcfa8296f
Jim Etevenard <jetevenard@oxiane.com>
2018-09-13 11:31:16 +0200

    Xsl Comment Rule activated in profile

--- src/main/java/labo/jim/sonar/xsl/extensions/XslProfile.java
+++ src/main/java/labo/jim/sonar/xsl/extensions/XslProfile.java
@@ -10,14 +10,10 @@ public class XslProfile implements BuiltInQualityProfilesDefinition{
 		NewBuiltInQualityProfile profile = context.createBuiltInQualityProfile("XSLT Rules Quality Porfile", XslLanguage.KEY);
 	    profile.setDefault(true);
 
-	    // TODO todoooooooo !!
 	    
-//	    NewBuiltInActiveRule rule1 = profile.activateRule(REPO_KEY, "ExampleRule1");
-//	    rule1.overrideSeverity("BLOCKER");
-//	    NewBuiltInActiveRule rule2 = profile.activateRule(REPO_KEY, "ExampleRule2");
-//	    rule2.overrideSeverity("MAJOR");
-//	    NewBuiltInActiveRule rule3 = profile.activateRule(REPO_KEY, "ExampleRule3");
-//	    rule3.overrideSeverity("CRITICAL");
+	    NewBuiltInActiveRule rule1 = profile.activateRule(XslRules.REPOSITORY, XslRules.SHOULD_HAVE_COMMENTS.rule());
+	    // rule1.overrideSeverity("BLOCKER");
+
 		
 	}
 
